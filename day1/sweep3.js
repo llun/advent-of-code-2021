fffcadedba8160a768f312c65cec34f071d9ae1f
Maythee Anegboonlap <null@llun.me>
2021-12-02 21:04:29 +0800

    Add common solution

--- day1/sweep3.js
+++ day1/sweep3.js
@@ -1,39 +1,51 @@
 const fs = require("fs");
 const path = require("path");
 
+const lines = fs
+  .readFileSync(path.join(__dirname, "input.txt"))
+  .toString("utf-8")
+  .trim()
+  .split("\n")
+  .map((item) => parseInt(item, 10));
+
+// Common
+function count(lines, diff) {
+  return lines
+    .map((_, index, arr) => arr[index + diff] - arr[index])
+    .filter((item) => item > 0).length;
+}
+
+// Part 1
 console.log(
-  fs
-    .readFileSync(path.join(__dirname, "input.txt"))
-    .toString("utf-8")
-    .trim()
-    .split("\n")
-    .map((item) => parseInt(item, 10))
+  lines
     .map((value, index, arr) => value - (arr[index - 1] || 0))
     .filter((item) => item > 0).length - 1
 );
 
 console.log(
-  fs
-    .readFileSync(path.join(__dirname, "input.txt"))
-    .toString("utf-8")
-    .trim()
-    .split("\n")
-    .map((item) => parseInt(item, 10))
-    .map((item, index, arr) => arr.slice(index, index + 3))
+  lines
+    .map((_, index, arr) => arr[index + 1] - arr[index])
+    .filter((item) => item > 0).length
+);
+
+console.log(count(lines, 1));
+
+// Part 2
+console.log(
+  lines
+    .map((_, index, arr) => arr.slice(index, index + 3))
     .filter((item) => item.length === 3)
     .map((items) => items.reduce((a, b) => a + b))
     .map((value, index, arr) => value - (arr[index - 1] || 0))
     .filter((item) => item > 0).length - 1
 );
 
+// Part 2 another solution
 console.log(
-  fs
-    .readFileSync(path.join(__dirname, "input.txt"))
-    .toString("utf-8")
-    .trim()
-    .split("\n")
-    .map((item) => parseInt(item, 10))
+  lines
     // B + C + D > A + B + C = D > A (both side have B + C)
     .map((_, index, arr) => arr[index + 3] - arr[index])
     .filter((item) => item > 0).length
 );
+
+console.log(count(lines, 3));
