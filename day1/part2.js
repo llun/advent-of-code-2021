4eef9799150c49d597454afea348ffe7ed169477
Maythee Anegboonlap <null@llun.me>
2021-12-02 14:12:36 +0800

    Add part2

---
+++ day1/part2.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+function window(size) {
+  return function (acc, item, i, arr) {
+    if (i > arr.length - size) return acc;
+    return [...acc, arr.slice(i, i + size)];
+  };
+}
+
+console.log(
+  fs
+    .readFileSync(path.join(__dirname, "input.txt"))
+    .toString("utf8")
+    .split("\n")
+    .map((line) => parseInt(line, 10))
+    .reduce(window(3), [])
+    .map((triple) => triple.reduce((a, i) => a + i, 0))
+    .reduce(window(2), [])
+    .reduce((acc, pair) => (pair[1] > pair[0] ? acc + 1 : acc), 0)
+);
