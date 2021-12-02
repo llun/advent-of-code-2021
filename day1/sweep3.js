8342662575f20a959d8d414d1cc344b76657163e
Maythee Anegboonlap <null@llun.me>
2021-12-02 21:00:17 +0800

    Add day1 part2 D > A solution

--- day1/sweep3.js
+++ day1/sweep3.js
@@ -25,3 +25,15 @@ console.log(
     .map((value, index, arr) => value - (arr[index - 1] || 0))
     .filter((item) => item > 0).length - 1
 );
+
+console.log(
+  fs
+    .readFileSync(path.join(__dirname, "input.txt"))
+    .toString("utf-8")
+    .trim()
+    .split("\n")
+    .map((item) => parseInt(item, 10))
+    // B + C + D > A + B + C = D > A (both side have B + C)
+    .map((_, index, arr) => arr[index + 3] - arr[index])
+    .filter((item) => item > 0).length
+);
