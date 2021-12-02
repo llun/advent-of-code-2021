14faf0834ae466afc0ed8217b9a3c027b00fc6d0
Maythee Anegboonlap <null@llun.me>
2021-12-02 10:01:03 +0800

    Add java version

---
+++ day1/index.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Index {
+    public static void main(String args[]) throws IOException {
+        var lines = Files.lines(Paths.get("day1/input.txt")).map(line -> Integer.parseInt(line, 10)).collect(Collectors.toList());
+        System.out.println(IntStream.range(0, lines.size() - 1).mapToObj(i -> List.of(lines.get(i), lines.get(i + 1))).reduce(0, (total, element) -> (element.get(1) > element.get(0)) ? total + 1 : total, Integer::sum));
+    }
+}
