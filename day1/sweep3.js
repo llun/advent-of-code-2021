3e6405a4a37377522235359092bfe651cf8ec778
Maythee Anegboonlap <null@llun.me>
2021-12-02 15:07:00 +0800

    Add part2 with filter

---
+++ day1/sweep3.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+
+console.log(
+  fs
+    .readFileSync(path.join(__dirname, "input.txt"))
+    .toString("utf-8")
+    .trim()
+    .split("\n")
+    .map((item) => parseInt(item, 10))
+    .map((value, index, arr) => value - (arr[index - 1] || 0))
+    .filter((item) => item > 0).length - 1
+);
+
+console.log(
+  fs
+    .readFileSync(path.join(__dirname, "input.txt"))
+    .toString("utf-8")
+    .trim()
+    .split("\n")
+    .map((item) => parseInt(item, 10))
+    .map((item, index, arr) => arr.slice(index, index + 3))
+    .filter((item) => item.length === 3)
+    .map((items) => items.reduce((a, b) => a + b))
+    .map((value, index, arr) => value - (arr[index - 1] || 0))
+    .filter((item) => item > 0).length - 1
+);
