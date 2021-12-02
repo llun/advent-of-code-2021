85cbb5279d8eb4ca21cfb1b500f15cb1b9b44655
Maythee Anegboonlap <null@llun.me>
2021-12-02 10:26:35 +0800

    A little refactor in Java

--- day1/index.java
+++ day1/index.java
@@ -7,7 +7,7 @@ import java.util.stream.IntStream;
 
 class Index {
     public static void main(String args[]) throws IOException {
-        var lines = Files.lines(Paths.get("day1/input.txt")).map(line -> Integer.parseInt(line, 10)).collect(Collectors.toList());
+        var lines = Files.lines(Paths.get("day1/input.txt")).map(Integer::parseInt).collect(Collectors.toList());
         System.out.println(IntStream.range(0, lines.size() - 1).mapToObj(i -> List.of(lines.get(i), lines.get(i + 1))).reduce(0, (total, element) -> (element.get(1) > element.get(0)) ? total + 1 : total, Integer::sum));
     }
 }
