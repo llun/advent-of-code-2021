1fbe908004278851d10f5bf117b6cb7fa98bb2da
Maythee Anegboonlap <null@llun.me>
2021-12-07 13:30:19 +0800

    Add part2

--- day7/whale.js
+++ day7/whale.js
@@ -19,3 +19,31 @@ const findLeastCost = (input) => {
   return leastCost;
 };
 console.log(findLeastCost(input));
+
+const data = new Map();
+const getCost = (n) => {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if (data.has(n)) return data.get(n);
+  const val = n + getCost(n - 1);
+  data.set(n, val);
+  return val;
+};
+
+const findLeastCost2 = (input) => {
+  const sortedInput = input.slice(0).sort((a, b) => a - b);
+  const min = sortedInput[0];
+  const max = sortedInput[sortedInput.length - 1];
+
+  let leastCost = Number.MAX_SAFE_INTEGER;
+  for (let i = min; i <= max; i++) {
+    const cost = input.reduce(
+      (sum, val) => sum + getCost(Math.abs(val - i)),
+      0
+    );
+    leastCost = cost < leastCost ? cost : leastCost;
+  }
+  return leastCost;
+};
+
+console.log(findLeastCost2(input));
