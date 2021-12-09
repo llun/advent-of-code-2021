3ef505caf6c9601a163cb0e34db88d090179f61a
Maythee Anegboonlap <null@llun.me>
2021-12-09 13:41:51 +0800

    Add part2

--- day9/smoke.js
+++ day9/smoke.js
@@ -42,3 +42,38 @@ const part1 = (input) => {
 console.log("Part1");
 console.log(part1(getInput("./sample.txt")));
 console.log(part1(getInput("./input.txt")));
+
+const part2 = (input) => {
+  const findBasin = (input, x, y) => {
+    if (x < 0) return 0;
+    if (y < 0) return 0;
+    if (x >= input[0].length) return 0;
+    if (y >= input.length) return 0;
+    if (input[y][x] === 9) return 0;
+
+    input[y][x] = 9;
+    return (
+      1 +
+      // Left
+      findBasin(input, x - 1, y) +
+      // Top
+      findBasin(input, x, y - 1) +
+      // Right
+      findBasin(input, x + 1, y) +
+      // Bottom
+      findBasin(input, x, y + 1)
+    );
+  };
+  const allBasins = input.reduce((all, row, y) => {
+    const basins = row.reduce((all, cell, x) => {
+      const size = findBasin(input, x, y);
+      return [...all, size];
+    }, []);
+    return [...all, ...basins];
+  }, []);
+  const sorted = allBasins.sort((a, b) => b - a);
+  return sorted.slice(0, 3).reduce((a, b) => a * b, 1);
+};
+
+console.log(part2(getInput("./sample.txt")));
+console.log(part2(getInput("./input.txt")));
