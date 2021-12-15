ea7aa4087fbcdbd53fce140cbc11d230401df590
Maythee Anegboonlap <null@llun.me>
2021-12-16 06:36:55 +0800

    Add heuristic function

--- day15/chiton.js
+++ day15/chiton.js
@@ -21,9 +21,7 @@ class Queue {
 
   shift() {
     const head = this.heap.extractMinimum();
-    const cost = parseInt(head.key);
-    const node = head.value;
-    return [cost, node];
+    return head.value;
   }
 
   get length() {
@@ -31,13 +29,19 @@ class Queue {
   }
 }
 
+const manhattan = (origin, target, cost) => {
+  const [x1, y1] = origin;
+  const [x2, y2] = target;
+  return (Math.abs(x2 - x1) + Math.abs(y2 - y1)) * cost;
+};
+
 const part1 = (input) => {
   const matrix = input;
   const start = [0, 0];
   const end = [matrix[0].length - 1, matrix.length - 1];
 
-  const getNeighbourNodeWithCost = (matrix, visited, node) => {
-    const [cost, position] = node;
+  const getNeighbourNodeWithCost = (matrix, visited, target, node) => {
+    const [cost, totalRisk, position] = node;
     const [x, y] = position;
 
     const top = y - 1 < 0 ? null : [x, y - 1];
@@ -49,22 +53,25 @@ const part1 = (input) => {
       .filter((item) => !visited.has(item.join("")))
       .map((item) => {
         const [x, y] = item;
-        return [matrix[y][x] + cost, item];
+        const h = manhattan(position, target, matrix[y][x]);
+        const f = matrix[y][x] + cost;
+        const n = f + h;
+        return [n, [n, totalRisk + matrix[y][x], item]];
       });
   };
 
   const queue = new Queue();
-  queue.push(0, start);
+  queue.push(0, [0, 0, start]);
   const visited = new Set();
 
   while (queue.length > 0) {
-    const node = queue.shift();
-    const [cost, coordinate] = node;
-    if (coordinate.join("") === end.join("")) {
-      return cost;
+    const head = queue.shift();
+    const [cost, totalRisk, position] = head;
+    if (position.join("") === end.join("")) {
+      return totalRisk;
     }
-    visited.add(coordinate.join(""));
-    const nodes = getNeighbourNodeWithCost(matrix, visited, node);
+    visited.add(position.join(""));
+    const nodes = getNeighbourNodeWithCost(matrix, visited, end, head);
     for (const node of nodes) {
       queue.push(...node);
     }
@@ -72,5 +79,32 @@ const part1 = (input) => {
 };
 
 console.log("Part1");
-console.log(part1(getInput("./sample.txt")));
+// console.log(part1(getInput("./sample.txt")));
 console.log(part1(getInput("./input.txt")));
+
+const part2 = (input) => {
+  const increaseInput = (input) =>
+    input.map((row) => row.map((cell) => cell + 1));
+
+  const joinMatrixLeft = (matrix1, matrix2) =>
+    matrix1.map((row, i) => [...row, ...matrix2[i]]);
+
+  const joinMatrixBottom = (matrix1, matrix2) => [...matrix1, ...matrix2];
+
+  let row = input;
+  let current = input;
+  for (let j = 0; j < 4; j++) {
+    current = increaseInput(current);
+    row = joinMatrixLeft(row, current);
+  }
+  current = row;
+  for (let i = 0; i < 4; i++) {
+    current = increaseInput(current);
+    row = joinMatrixBottom(row, current);
+  }
+  return part1(row);
+};
+
+console.log("Part2");
+// console.log(part2(getInput("./sample.txt")));
+// console.log(part2(getInput("./input.txt")));
